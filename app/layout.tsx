5bbccff24aa11ec82010da67ddde798de87eb244
vulgoneymkt-lab <vulgoneymkt@gmail.com>
2025-08-19 17:41:15 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+<script>
+  window.pixelId = "68a4de217ebd96dd10131540";
+  var a = document.createElement("script");
+  a.setAttribute("async", "");
+  a.setAttribute("defer", "");
+  a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
+  document.head.appendChild(a);
+</script>
+
+<!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '2167725977035864');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=2167725977035864&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
         <style>{`
 html {
   font-family: ${GeistSans.style.fontFamily};
